b08c692c38516eee40ef6d09a99f524f0ee35fae
Step Schwarz <step@closethipster.com>
2020-09-21 02:39:24 -0400

    Completes tasks from Challenge 1

--- src/example1/example1.js
+++ src/example1/example1.js
@@ -4,10 +4,23 @@ rh.test.flip = {
         trigger: "data-test-flip-on",
         visible: "data-test-visible"
     },
-    action: function( $el ) {}
+    action: function( $el ) {
+        // Determine which card's button was clicked...
+        // ...and find it's opposite face (front if back, back if front)
+        var $thisCard = $el.closest( ".test-card" ),
+            $oppositeFace = ( $thisCard.hasClass( "front" ) ) ? $thisCard.next() : $thisCard.prev();
+
+        // The "visible" attribute is defined, so let's use it
+        $thisCard.removeAttr( rh.test.flip.attr.visible );
+        $oppositeFace.attr( rh.test.flip.attr.visible, "" );
+    }
 };
 
 // For all flip-enabled elements, attach an on click or on hover event
 $( "[" + rh.test.flip.attr.trigger + "]", context ).each( function( idx, val ) {
     // Add your trigger event here
+    $( val ).on( "click", function( evt ) {
+        evt.preventDefault();
+        rh.test.flip.action( $( this ) );
+    } );
 } );
